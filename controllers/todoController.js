757134425e6c9f89c733b2f80d2cc3a1d325d84b
Mihaja Razafimahefa <mihaja.razafimahefa@gmail.com>
2019-08-15 13:58:20 +0300

    add submit form

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,9 +1,18 @@
+/* eslint-disable no-console */
+const bodyParser = require('body-parser');
+
+const urlencodedParser = bodyParser.urlencoded({ extended: false });
+const data = [{ item: 'learn javascript' }, { item: 'learn node.js' }, { item: 'learn websocket' }];
+
 module.exports = app => {
     app.get('/todo', (req, res) => {
-        res.render('todo');
+        res.render('todo', { todos: data });
     });
 
-    app.post('/todo', (req, res) => {});
+    app.post('/todo', urlencodedParser, (req, res) => {
+        data.push(req.body);
+        res.json(data);
+    });
 
     app.delete('/todo', (req, res) => {});
 };
--- public/assets/todo-list.js
+++ public/assets/todo-list.js
@@ -1,34 +1,32 @@
-$(document).ready(function(){
+$(document).ready(function() {
+    $('form').on('submit', function() {
+        const item = $('form input');
+        const todo = { item: item.val() };
 
-  $('form').on('submit', function(){
+        $.ajax({
+            type: 'POST',
+            url: '/todo',
+            data: todo,
+            success(data) {
+                // do something with the data via front-end framework
+                location.reload();
+            },
+        });
 
-      var item = $('form input');
-      var todo = {item: item.val()};
+        return false;
+    });
 
-      $.ajax({
-        type: 'POST',
-        url: '/todo',
-        data: todo,
-        success: function(data){
-          //do something with the data via front-end framework
-          location.reload();
-        }
-      });
-
-      return false;
-
-  });
-
-  $('li').on('click', function(){
-      var item = $(this).text().replace(/ /g, "-");
-      $.ajax({
-        type: 'DELETE',
-        url: '/todo/' + item,
-        success: function(data){
-          //do something with the data via front-end framework
-          location.reload();
-        }
-      });
-  });
-
+    $('li').on('click', function() {
+        const item = $(this)
+            .text()
+            .replace(/ /g, '-');
+        $.ajax({
+            type: 'DELETE',
+            url: `/todo/${item}`,
+            success(data) {
+                // do something with the data via front-end framework
+                location.reload();
+            },
+        });
+    });
 });
